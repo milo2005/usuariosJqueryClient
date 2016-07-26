fd9afe9b604534acbcea18257b50e57aee957a05
System Administrator <root@MacBook-Pro-de-Portatil-3.local>
2016-07-26 16:28:38 -0500

    Proyecto creado

---
+++ src/js/logic2.js
@@ -0,0 +1,4 @@
+$ = require("jquery");
+$("#btn2").click(function(){
+    alert("Presionaste el boton 2");
+});
---
+++ src/js/logic3.js
@@ -0,0 +1,4 @@
+$ = require("jquery");
+$("#btn3").click(function(){
+    alert("Presionaste el boton 3");
+});
