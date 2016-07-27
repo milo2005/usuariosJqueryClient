71a60401bf9ae245c3f19345ed3f860686e7182b
System Administrator <root@MacBook-Pro-de-Portatil-3.local>
2016-07-27 17:13:50 -0500

    template fixed

--- src/js/logic-usuarios.js
+++ src/js/logic-usuarios.js
@@ -1,2 +1,11 @@
 var crypto = require("crypto-js");
 
+$("#btn-txt-in").click(function(){ openModal("login"); });
+$("#btn-icon-in").click(function(){ openModal("login"); });
+
+function openModal(modal){
+    $("#"+modal).openModal();
+}
+
+
+
