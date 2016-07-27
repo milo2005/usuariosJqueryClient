a9cb3b1d3a7cba694215cdabd1d80152db62cacb
System Administrator <root@MacBook-Pro-de-Portatil-3.local>
2016-07-27 17:56:15 -0500

    logic-usuarios fixed

--- src/js/logic-usuarios.js
+++ src/js/logic-usuarios.js
@@ -7,5 +7,33 @@ function openModal(modal){
     $("#"+modal).openModal();
 }
 
+$("#btn-reg").click(function(){
+    var nombre = $("#name").val();
+    var cedula = $("#ced").val();
+    var genero = $("input[name='genero']:checked").val();
+    var usuario = $("#usr").val();
+    var pass = $("#pass").val();
+
+    var auth = usuario+"&&"+pass;
+    auth = ""+crypto.AES.encrypt(auth,"cualquiercosa");
+
+    var usr = {nombre: nombre, cedula: cedula, genero: genero, auth: auth};
+
+    console.log(JSON.stringify(usr));
+
+    $.ajax({
+        method:"POST",
+        data:usr,
+        datatype:"json",
+        url:"http://localhost:8080/api/usuarios"
+    }).done(function(res){
+        Materialize.toast("Registro exitoso",4000);
+    }).fail(function(){
+        Materialize.toast("Error al registrar usuario",4000);
+    });
+
+
+});
+
 
 
